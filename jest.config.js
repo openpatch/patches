d011f21b79994c656303801e47ab87b5e3316bc7
Mike Barkmin <mike@barkmin.eu>
2020-11-24 21:09:59 +0100

    chore: better jest config for coverage

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,16 @@ module.exports = {
   testEnvironment: "jsdom",
   snapshotSerializers: ["@emotion/jest/serializer"],
   testPathIgnorePatterns: ["<rootDir>/dist/", "<rootDir>/node_modules/"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+  transform: { ".(ts|tsx)$": "ts-jest/dist" },
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+    },
   },
   collectCoverageFrom: [
     "src/**/*.{tsx, ts, js, jsx}",
+    "!src/private/**/*",
     "!src/icons/**/*.{tsx, ts, js, jsx}",
     "!src/**/*stories.{tsx, ts, js, jsx}",
     "!src/**/*test.{tsx, ts, js, jsx}",
