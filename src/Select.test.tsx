20897b37d3e25c07a601cce2ef8dfd1628014780
Mike Barkmin <mike@barkmin.eu>
2021-03-04 15:55:59 +0100

    feat(select): forward ref

--- src/Select.test.tsx
+++ src/Select.test.tsx
@@ -1,20 +1,28 @@
-
 import { render } from "@testing-library/react";
+import { createRef } from "react";
 import { Select, SelectProps } from "./Select";
 import { ThemeProvider } from "./ThemeProvider";
 
 const renderWithTheme = (props?: SelectProps) =>
-render(
+  render(
     <ThemeProvider>
-        <Select {...props} />
+      <Select {...props} />
     </ThemeProvider>
-);
+  );
 
 test("should render", () => {
-   renderWithTheme();
+  renderWithTheme();
 });
 
 test("should match snapshot", () => {
-    const { container } = renderWithTheme();
-    expect(container).toMatchSnapshot();
+  const { container } = renderWithTheme();
+  expect(container).toMatchSnapshot();
+});
+
+test("should have ref", () => {
+  const ref = createRef<HTMLSelectElement>();
+  renderWithTheme({ ref });
+
+  expect(ref.current).not.toBeNull();
+  expect(ref.current.value).toBe("");
 });
--- src/Select.tsx
+++ src/Select.tsx
@@ -1,4 +1,10 @@
-import { ChangeEvent, InputHTMLAttributes, ReactNode } from "react";
+import {
+  ChangeEvent,
+  forwardRef,
+  ReactNode,
+  RefObject,
+  SelectHTMLAttributes,
+} from "react";
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
 
@@ -27,13 +33,14 @@ export type SelectProps = {
   children?: ReactNode;
   value?: string;
   error?: boolean;
+  ref?: RefObject<HTMLSelectElement>;
   onChange?: (value: string) => void;
   onBlur?: () => void;
   disbaled?: boolean;
   required?: boolean;
 };
 
-type StyledSelectProps = InputHTMLAttributes<HTMLSelectElement> &
+type StyledSelectProps = SelectHTMLAttributes<HTMLSelectElement> &
   ColorProps &
   CursorProps &
   SpaceProps &
@@ -46,85 +53,93 @@ const StyledSelect = styled.select<StyledSelectProps>(
   compose(color, cursor, space, typography, background, border, shadow)
 );
 
-export const Select = ({
-  id,
-  children,
-  onChange = () => {},
-  onBlur = () => {},
-  disbaled = false,
-  value,
-  error = false,
-  required = false,
-}: SelectProps) => {
-  function handleSelect(e: ChangeEvent<HTMLSelectElement>) {
-    if (!disbaled) {
-      const newValue = e.currentTarget.value;
-      onChange(newValue);
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+  (
+    {
+      id,
+      children,
+      onChange = () => {},
+      onBlur = () => {},
+      disbaled = false,
+      value,
+      error = false,
+      required = false,
+      ...props
+    },
+    ref
+  ) => {
+    function handleSelect(e: ChangeEvent<HTMLSelectElement>) {
+      if (!disbaled) {
+        const newValue = e.currentTarget.value;
+        onChange(newValue);
+      }
     }
-  }
-  return (
-    <Box width="100%" position="relative">
-      <StyledSelect
-        id={id}
-        required={required}
-        borderRadius="small"
-        padding="xsmall"
-        borderColor="neutral.100"
-        borderStyle="solid"
-        onChange={handleSelect}
-        onBlur={onBlur}
-        value={value}
-        disabled={disbaled}
-        borderWidth="standard"
-        aria-invalid={error}
-        css={(theme) => [
-          css`
-            width: 100%;
-            appearance: none;
-            outline-color: transparent;
-            outline-width: 0;
-            background: white;
-
-            ::placeholder {
-              color: ${theme.colors.neutral["400"]};
-            }
-            :focus {
+    return (
+      <Box width="100%" position="relative">
+        <StyledSelect
+          {...props}
+          id={id}
+          required={required}
+          borderRadius="small"
+          padding="xsmall"
+          borderColor="neutral.100"
+          borderStyle="solid"
+          onChange={handleSelect}
+          onBlur={onBlur}
+          value={value}
+          ref={ref}
+          disabled={disbaled}
+          borderWidth="standard"
+          aria-invalid={error}
+          css={(theme) => [
+            css`
+              width: 100%;
+              appearance: none;
               outline-color: transparent;
               outline-width: 0;
-              border-color: ${theme.colors.primary["800"]};
-              box-shadow: ${theme.shadows.outline};
-              z-index: 1;
-            }
-          `,
-          disbaled &&
-            css`
-              cursor: ${theme.cursor["not-allowed"]};
-              background-color: ${theme.colors.neutral["50"]};
+              background: white;
+
+              ::placeholder {
+                color: ${theme.colors.neutral["400"]};
+              }
+              :focus {
+                outline-color: transparent;
+                outline-width: 0;
+                border-color: ${theme.colors.primary["800"]};
+                box-shadow: ${theme.shadows.outline};
+                z-index: 1;
+              }
             `,
-          error &&
-            css`
-              border-color: ${theme.colors.error["500"]};
-            `,
-        ]}
-      >
-        {children}
-      </StyledSelect>
-      <Box
-        position="absolute"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        width="32px"
-        right="8px"
-        top="0"
-        bottom="0"
-        css={css`
-          pointer-events: none;
-        `}
-        tabIndex={-1}
-      >
-        <CheveronDown />
+            disbaled &&
+              css`
+                cursor: ${theme.cursor["not-allowed"]};
+                background-color: ${theme.colors.neutral["50"]};
+              `,
+            error &&
+              css`
+                border-color: ${theme.colors.error["500"]};
+              `,
+          ]}
+        >
+          {children}
+        </StyledSelect>
+        <Box
+          position="absolute"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          width="32px"
+          right="8px"
+          top="0"
+          bottom="0"
+          css={css`
+            pointer-events: none;
+          `}
+          tabIndex={-1}
+        >
+          <CheveronDown />
+        </Box>
       </Box>
-    </Box>
-  );
-};
+    );
+  }
+);
