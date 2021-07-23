2fe6f88fd7ec48c2e800a52133c2d8f1553f5f16
Mike Barkmin <mike@barkmin.eu>
2021-07-23 15:17:29 +0200

    feat(text): add lineClamp to text

--- src/Text.tsx
+++ src/Text.tsx
@@ -2,23 +2,32 @@ import { css } from "@emotion/react";
 import { Properties } from "csstype";
 import { ReactNode } from "react";
 import { Box } from "./Box";
+import { useBreakpoints } from "./hooks";
 import { ColorProps, SpaceProps, TypographyProps } from "./system";
-import { SystemElements } from "./system/types";
+import { ResponsiveProp, SystemElements } from "./system/types";
 
 export type TextProps = {
   id?: string;
   htmlFor?: string;
   as?: SystemElements;
   truncate?: Properties["width"];
+  lineClamp?: ResponsiveProp<number>;
   children?: ReactNode;
   href?: string;
   className?: string;
   role?: string;
+  title?: string;
 } & TypographyProps &
   SpaceProps &
   Pick<ColorProps, "textColor">;
 
-export const Text = ({ truncate, as = "p", ...props }: TextProps) => {
+export const Text = ({
+  truncate,
+  lineClamp,
+  as = "p",
+  ...props
+}: TextProps) => {
+  const responsiveLineClamp = useBreakpoints(lineClamp);
   return (
     <Box
       p="none"
@@ -26,6 +35,13 @@ export const Text = ({ truncate, as = "p", ...props }: TextProps) => {
       display={as == "span" ? "inline" : undefined}
       as={as}
       css={[
+        lineClamp &&
+          css`
+            display: -webkit-box;
+            -webkit-box-orient: vertical;
+            -webkit-line-clamp: ${responsiveLineClamp};
+            overflow: hidden;
+          `,
         truncate &&
           css`
             display: inline-block;
--- src/hooks/useBreakpoints.tsx
+++ src/hooks/useBreakpoints.tsx
@@ -1,3 +1,4 @@
+import { ResponsiveProp } from "../system/types";
 import { useMedia } from "./useMedia";
 import { useTheme } from "./useTheme";
 
@@ -5,9 +6,14 @@ const makeQuery = (breakpoint: string) => {
   return `(min-width: ${breakpoint})`;
 };
 
-export const useBreakpoints = <T,>(values: T[]) => {
+export const useBreakpoints = <T,>(values: ResponsiveProp<T>) => {
   const [theme] = useTheme();
   const queries = theme.breakpoints.map((b) => makeQuery(b.toString()));
+  if (!values) {
+    values = [];
+  } else if (!Array.isArray(values)) {
+    values = [values];
+  }
   // mutations are bad. Therefore, we make a copy.
   const copyValues = [...values];
   const defaultValue = copyValues.shift();
