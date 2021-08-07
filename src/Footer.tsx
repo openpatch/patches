6d2110eb5aa57757f6b118d401fee669bd6c9b77
Mike Barkmin <mike@barkmin.eu>
2021-08-08 01:08:01 +0200

    fix(footer): use max-width to center the footer
    This is needed to be in-line with the nav and the main component

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -12,62 +12,64 @@ export type FooterProps = {
 
 export const Footer = ({ links, copyright = "OpenPatch" }: FooterProps) => {
   return (
-    <Box
-      as="footer"
-      px={["small", "medium", "medium"]}
-      py={["xsmall", "small"]}
-      backgroundColor="primary.900"
-    >
-      {links && (
-        <Box as="nav" aria-label="footer-menu">
-          <ul
-            css={(theme) => css`
-              list-style: none;
-              display: flex;
-              align-items: center;
-              padding: 0;
-              margin-top: 0;
-              margin-bottom: ${theme.space.xxsmall};
-              flex-wrap: wrap;
-              margin-left: -${theme.space.xsmall};
-            `}
-          >
-            {links?.map(({ href, label }) => (
-              <li
-                key={label}
-                css={css`
-                  margin: 4px;
-                `}
-              >
-                <Box
-                  as="a"
-                  fontWeight="semibold"
-                  borderRadius="standard"
-                  fontSize="small"
-                  padding="xsmall"
-                  css={(theme) => css`
-                    text-decoration: none;
-
-                    :hover {
-                      color: ${theme.colors.primary["50"]};
-                      background-color: ${theme.colors.primary["800"]};
-                    }
+    <Box as="footer" backgroundColor="primary.900">
+      <Box
+        maxWidth="large"
+        mx="auto"
+        px={["small", "medium", "medium"]}
+        py={["xsmall", "small"]}
+      >
+        {links && (
+          <Box as="nav" aria-label="footer-menu">
+            <ul
+              css={(theme) => css`
+                list-style: none;
+                display: flex;
+                align-items: center;
+                padding: 0;
+                margin-top: 0;
+                margin-bottom: ${theme.space.xxsmall};
+                flex-wrap: wrap;
+                margin-left: -${theme.space.xsmall};
+              `}
+            >
+              {links?.map(({ href, label }) => (
+                <li
+                  key={href}
+                  css={css`
+                    margin: 4px;
                   `}
-                  textColor="primary.200"
-                  href={href}
                 >
-                  {label}
-                </Box>
-              </li>
-            ))}
-          </ul>
-        </Box>
-      )}
-      {copyright && (
-        <Box fontSize="small" textColor="primary.300" fontWeight="semibold">
-          &copy; {new Date().getFullYear()} {copyright}
-        </Box>
-      )}
+                  <Box
+                    as="a"
+                    fontWeight="semibold"
+                    borderRadius="standard"
+                    fontSize="small"
+                    padding="xsmall"
+                    css={(theme) => css`
+                      text-decoration: none;
+
+                      :hover {
+                        color: ${theme.colors.primary["50"]};
+                        background-color: ${theme.colors.primary["800"]};
+                      }
+                    `}
+                    textColor="primary.200"
+                    href={href}
+                  >
+                    {label}
+                  </Box>
+                </li>
+              ))}
+            </ul>
+          </Box>
+        )}
+        {copyright && (
+          <Box fontSize="small" textColor="primary.300" fontWeight="semibold">
+            &copy; {new Date().getFullYear()} {copyright}
+          </Box>
+        )}
+      </Box>
     </Box>
   );
 };
