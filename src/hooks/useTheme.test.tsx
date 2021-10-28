f01140e83f1814559f3b9b9e968b8ce5f0fd47b8
Mike Barkmin <mike@barkmin.eu>
2021-10-28 18:59:24 +0200

    fix(use-theme): use theme not returning the correct theme

--- src/hooks/useTheme.test.tsx
+++ src/hooks/useTheme.test.tsx
@@ -1,39 +1,16 @@
+import { renderHook } from "@testing-library/react-hooks";
 import React, { ReactNode } from "react";
-import { renderHook, act } from "@testing-library/react-hooks";
-import { baseTheme } from "../themes";
-import { red } from "../themes/colors";
-import { useTheme } from "./useTheme";
 import { ThemeProvider } from "../ThemeProvider";
+import { baseTheme } from "../themes";
+import { useTheme } from "./useTheme";
 
 const wrapper = ({ children }: { children: ReactNode }) => (
   <ThemeProvider>{children}</ThemeProvider>
 );
-
-test("should use default theme", () => {
-  const { result } = renderHook(() => useTheme({ colors: { primary: red } }), {
-    wrapper,
-  });
-
-  expect(result.current[0].colors.primary).toEqual(red);
-});
 
 test("should return base", () => {
   const { result } = renderHook(() => useTheme(), { wrapper });
 
   expect(result.current.length).toBe(2);
   expect(result.current[0]).toEqual(baseTheme);
-});
-
-test("should change theme", () => {
-  const { result } = renderHook(() => useTheme(), { wrapper });
-
-  act(() => {
-    result.current[1]({
-      colors: {
-        primary: red,
-      },
-    });
-  });
-
-  expect(result.current[0].colors.primary).toEqual(red);
 });
