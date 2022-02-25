ad7ce1e62a9b280d2b795d9fd194428f7b0781cb
Mike Barkmin <mike@barkmin.eu>
2022-02-25 10:11:33 +0100

    fix(textlink): make detection of external link work with ssr

--- src/TextLink.tsx
+++ src/TextLink.tsx
@@ -1,5 +1,5 @@
 import { css } from "@emotion/react";
-import { forwardRef, ReactNode } from "react";
+import { forwardRef, ReactNode, useEffect, useState } from "react";
 import { useLinkComponent } from "./hooks";
 import { Icon } from "./Icon";
 import { ExternalWindow } from "./icons/shade";
@@ -9,37 +9,45 @@ export type TextLinkProps = {
   children?: ReactNode;
 } & LinkComponentProps;
 
-
 // https://stackoverflow.com/a/6238456
-function isExternal(url: string): boolean {
-  var match = url.match(
-    /^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/
-  );
-  if (match) {
-    if (
-      typeof match[1] === "string" &&
-      match[1].length > 0 &&
-      match[1].toLowerCase() !== location.protocol
-    )
-      return true;
-    if (
-      typeof match[2] === "string" &&
-      match[2].length > 0 &&
-      match[2].replace(
-        new RegExp(
-          ":(" + { "http:": 80, "https:": 443 }[location.protocol] + ")?$"
-        ),
-        ""
-      ) !== location.host
-    )
-      return true;
-  }
-  return false;
+function useIsExternal(url: string): boolean {
+  const [isExternal, setIsExternal] = useState(false);
+
+  useEffect(() => {
+    const match = url.match(
+      /^([^:\/?#]+:)?(?:\/\/([^\/?#]*))?([^?#]+)?(\?[^#]*)?(#.*)?/
+    );
+    if (match) {
+      if (
+        typeof match[1] === "string" &&
+        match[1].length > 0 &&
+        match[1].toLowerCase() !== location.protocol
+      ) {
+        setIsExternal(true);
+      } else if (
+        typeof match[2] === "string" &&
+        match[2].length > 0 &&
+        match[2].replace(
+          new RegExp(
+            ":(" + { "http:": 80, "https:": 443 }[location.protocol] + ")?$"
+          ),
+          ""
+        ) !== location.host
+      ) {
+        setIsExternal(true);
+      }
+    } else {
+      setIsExternal(false);
+    }
+  }, [url]);
+
+  return isExternal;
 }
 
 export const TextLink = forwardRef<HTMLAnchorElement, TextLinkProps>(
   ({ children, href, color, ...props }, ref) => {
     const LinkComponent = useLinkComponent(ref);
+    const isExternal = useIsExternal(href);
     return (
       <LinkComponent
         css={(theme) => css`
@@ -56,7 +64,7 @@ export const TextLink = forwardRef<HTMLAnchorElement, TextLinkProps>(
         {...props}
       >
         {children}
-        {isExternal(href) && (
+        {isExternal && (
           <Icon size="auto" color="currentColor">
             <ExternalWindow />
           </Icon>
